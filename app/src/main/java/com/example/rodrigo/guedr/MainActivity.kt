cfc5b8bf1d0bbe3132fecd68718636d8bf0097ad
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-10-31 15:00:54 +0100

    Example of Kotlin language, savedInstanceState and Log

--- app/src/main/java/com/example/rodrigo/guedr/MainActivity.kt
+++ app/src/main/java/com/example/rodrigo/guedr/MainActivity.kt
@@ -12,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v(TAG, "He pasado por onCreate")
+
+        if (savedInstanceState != null) {
+            Log.v(TAG,  "saedInstanceState no es null y clave vale: ${savedInstanceState.getString("clave")}")
+        }
+        else {
+            Log.v(TAG, "savedInstanceState ES null")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "He pasado por onSaveInstanceState")
+
+//        if (outState != null) {
+//            outState.putString("clave", "valor")
+//        }
+
+        outState?.putString("clave", "valor")
     }
 }
