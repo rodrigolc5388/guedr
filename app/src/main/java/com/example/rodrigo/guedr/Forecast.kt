77861a0349b017cb4e1dcea43939c769e2f0fe1f
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-11-02 11:17:39 +0100

    MainActivityForecast interface created; ImageView, descriptionView, temps and humidity views added; views alignment + margins

---
+++ app/src/main/java/com/example/rodrigo/guedr/Forecast.kt
@@ -0,0 +1,3 @@
+package com.example.rodrigo.guedr
+
+data class Forecast(val maxTemp: Float, val minTemp: Float, val humidity: Float, val description: String, val icon: Int)
--- app/src/main/java/com/example/rodrigo/guedr/MainActivity.kt
+++ app/src/main/java/com/example/rodrigo/guedr/MainActivity.kt
@@ -7,63 +7,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     val TAG = MainActivity::class.java.canonicalName
-
-    var offlineWeatherImage: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.stone_button).setOnClickListener(this)
-        findViewById<Button>(R.id.donkey_button).setOnClickListener(this)
+        val forecast = Forecast(25f, 10f, 35f, "Soleado con alguna nube", R.drawable.ico_01)
 
-        offlineWeatherImage = findViewById(R.id.offline_weather_image)
-
-        Log.v(TAG, "He pasado por onCreate")
-
-        if (savedInstanceState != null) {
-            Log.v(TAG,  "saedInstanceState no es null y clave vale: ${savedInstanceState.getString("clave")}")
-        }
-        else {
-            Log.v(TAG, "savedInstanceState ES null")
-        }
-    }
-
-    override fun onClick(v: View?) {
-        Log.v(TAG, "Hemos pasado por onClick")
-
-
-        // La forma más "Kotlin" de hacer esto
-        Log.v(TAG, when (v?.id) {
-            R.id.stone_button -> "Han pulsado el botón piedra"
-            R.id.donkey_button -> "Han pulsado el botón burro"
-            else -> "No sé qué me han pulsado"
-        })
-
-//        when (v?.id) {
-//            R.id.stone_button -> offlineWeatherImage?.setImageResource(R.drawable.offline_weather)
-//            R.id.donkey_button -> offlineWeatherImage?.setImageResource(R.drawable.offline_weather2)
-//        }
-
-        offlineWeatherImage?.setImageResource(when (v?.id) {
-            R.id.donkey_button -> R.drawable.offline_weather2
-            else -> R.drawable.offline_weather
-        })
-
+        setForecast(forecast)
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        Log.v(TAG, "He pasado por onSaveInstanceState")
+    private fun setForecast(forecast: Forecast) {
 
-//        if (outState != null) {
-//            outState.putString("clave", "valor")
-//        }
-
-        outState?.putString("clave", "valor")
     }
+
 }
