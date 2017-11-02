a66e5fa88a13ed9571ab3be1b971d8b3c6b399da
Rodrigo Limpias <Rodrigo@MacBook-Pro-de-Rodrigo.local>
2017-11-02 23:36:44 +0100

    Basic example of starActivityForResult (WIP)

--- app/src/main/java/com/example/rodrigo/guedr/ForecastActivity.kt
+++ app/src/main/java/com/example/rodrigo/guedr/ForecastActivity.kt
@@ -1,7 +1,12 @@
 package com.example.rodrigo.guedr
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -29,6 +34,7 @@ class ForecastActivity : AppCompatActivity() {
                 }
         }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
@@ -36,6 +42,43 @@ class ForecastActivity : AppCompatActivity() {
         forecast = Forecast(25f, 10f, 35f, "Soleado con alguna nube", R.drawable.ico_01)
     }
 
+
+    // Este método define qué opciones de menú tenemos
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.settings, menu)
+
+        return true
+    }
+
+
+    // Este método dice qué se hace una vez que se ha pulsado una opción de menú
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.menu_show_settings) {
+            // Aquí sabemos que se ha pulsado la opción de menú de mostrar ajustes
+            val intent = Intent(this, SettingsActivity::class.java)
+            // Esto lo haríamos si la segunda pantalla no nos tiene que devolver nada
+            //startActivity(intent)
+
+            // Esto lo hacemos porque la segunda pantalla nos tiene que devolver unos valores
+            startActivityForResult(intent, 1)
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK){
+                Log.v ("TAG", "Soy ForecastActivity y han pulsado OK")
+            } else {
+                Log.v ("TAG", "Soy ForecastActivity y han pulsado CANCEL")
+            }
+        }
+    }
 
 
 }
--- app/src/main/java/com/example/rodrigo/guedr/SettingsActivity.kt
+++ app/src/main/java/com/example/rodrigo/guedr/SettingsActivity.kt
@@ -1,13 +1,51 @@
 package com.example.rodrigo.guedr
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.RadioGroup
 
 
 class SettingsActivity: AppCompatActivity() {
 
+    var radioGroup: RadioGroup? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // Esto sería un equivalente a una clase anónima en Kotlin
+//        findViewById<View>(R.id.ok_button).setOnClickListener(object: View.OnClickListener {
+//            override  fun onClick(v: View?){
+//                // Aquí iría el código de aceptar
+//                acceptSettings()
+//            }
+//        })
+
+
+        // Esto es una closure indicando v (una View) y no devuelve nada
+//        findViewById<View>(R.id.ok_button).setOnClickListener { v ->
+//            acceptSettings()
+//        }
+
+        findViewById<View>(R.id.ok_button).setOnClickListener { acceptSettings() }
+        findViewById<View>(R.id.cancel_button).setOnClickListener { cancelSettings() }
+
+        radioGroup = findViewById(R.id.units_rg)
     }
+
+
+    private fun cancelSettings(){
+        setResult(Activity.RESULT_CANCELED)
+        // Finalizamos esta actividad, regresando a la anterior
+        finish()
+    }
+
+    private fun acceptSettings(){
+        setResult(Activity.RESULT_OK)
+        // Finalizamos esta actividad, regresando a la anterior
+        finish()
+    }
+
 }
